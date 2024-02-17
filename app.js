5d9f34e30c19e5276e5560eeafd2d2989ec87e6e
Krishna Kumar <117253066+Krishu7827@users.noreply.github.com>
2024-02-17 09:48:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,154 +1,159 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const express = require('express')
+const {connect} = require('./db.config')
+const multer = require('multer')              // multer will be used to handle the form data.
+const Aws = require('aws-sdk')     
+const {SNSClient,PublishCommand} = require('@aws-sdk/client-sns')           // aws-sdk library will used to upload image to s3 bucket.
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+const {Product} = require('./Product.Schema')
+const fs = require('fs')
 require('dotenv').config()
-const app = express();
-const port = process.env.PORT || 3001;
-const url = require('url');
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-function extractWebsiteName(domain) {
-  const domainParts = domain.split('.');
-  if (domainParts.length > 2) {
-    // Remove the TLD and subdomain, if any
-    domainParts.splice(0, domainParts.length - 2);
-  }
-  return domainParts.join('.');
+const app = express()
+
+let FileFormat = ''
+// creating the storage variable to upload the file and providing the destination folder, 
+// if nothing is provided in the callback it will get uploaded in main directory
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, cb) {
+        cb(null, '')
+    }
+})
+
+// below variable is define to check the type of file which is uploaded
+
+const filefilter = (req, file, cb) => {
+    console.log(file.mimetype)
+   
+    FileFormat = file.mimetype
+        cb(null, true)
+  
 }
 
+// defining the upload variable for the configuration of photo being uploaded
+const upload = multer({ storage: storage, fileFilter: filefilter });
 
+// Now creating the S3 instance which will be used in uploading photo to s3 bucket.
+const s3 = new Aws.S3({
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,              // accessKeyId that is stored in .env file
+    secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       // secretAccessKey is also store in .env file
+})
 
-// Nodemailer configuration for SMTP
-const transporter = nodemailer.createTransport({
- service:"gmail",
-  auth: {
-    user: 'gautamsolar.vidoes01@gmail.com', // Replace with your SMTP username
-    pass: 'cxmxypwbaupolgqo', // Replace with your SMTP password or app-specific password
-  },
-  tls:{
-    rejectUnauthorized:false
-  }
-});
-
-// Route to handle form submission
-app.post("/submit-contactus", async (req, res) => {
-  try {
-    const formData = req.body;
-    const referrerUrl = req.headers.referer || "Unknown"; // Get the referrer URL
-    const referrerDomain = url.parse(referrerUrl).hostname; // Extract the domain name from the URL
-    const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
-
-    const checkboxValues = Object.keys(formData).filter(key => formData[key] === 'Yes');
-    const checkboxList = checkboxValues.map(key => `<li>${key}: Yes</li>`).join('');
-
-    // Construct the email content
-    const mailOptions = {
-      from: 'gautamsolar.vidoes01@gmail.com',
-      to: 'jokerkali99@gmail.com',
-      subject: 'New Contact Us Form Submission',
-      html: `
-      <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
-      <h2 style="color: #a20000;">New Contact Us Form Submission</h2>
-      <p style="margin-bottom: 10px;"><strong>Name:</strong> ${formData.name}</p>
-      <p style="margin-bottom: 10px;"><strong>Email:</strong> ${formData.email}</p>
-      <p style="margin-bottom: 10px;"><strong>Phone:</strong> ${formData.phone}</p>
-      <p style="margin-bottom: 10px;"><strong>City:</strong> ${formData.city}</p>
-      <p style="margin-bottom: 10px;"><strong>Remarks:</strong> ${formData.remark}</p>
-      <p style="margin-bottom: 10px;"><strong>MWp Requird:</strong></p>
-      <ul style="list-style-type: none; padding: 0;">
-        ${checkboxList}
-      </ul>
-      <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-    </div>
-      `,
+// now how to handle the post request and to upload photo (upload photo using the key defined below in upload.single ie: productimage )
+app.post('/',  upload.single('productimage'),async (req, res) => {
+    console.log(req.file)  // to check the data in the console that is being uploaded
+  
+    // Definning the params variable to uplaod the photo
+    
+    const params = {
+        Bucket:process.env.AWS_BUCKET_NAME,      // bucket that we made earlier
+        Key:req.file.originalname,               // Name of the image
+        Body:req.file.buffer,                    // Body which will contain the image in buffer format
+        ACL:"public-read-write",                 // defining the permissions to get the public link
+        ContentType:FileFormat                // Necessary to define the image content-type to view the photo in the browser with the link
     };
-    await transporter.sendMail(mailOptions);
+  
+   // uplaoding the photo using s3 instance and saving the link in the database.
+    
+    s3.upload(params,async(error,data)=>{
+        if(error){
+            res.status(500).send({"err":error})  // if we get any error while uploading error message will be returned.
+        }
+  
+   // If not then below code will be executed
+        
+    console.log(data)                      // this will give the information about the object in which photo is stored 
+    
+   // saving the information in the database.   
+    const product = new Product({
+            name: req.query.name,
+            price: req.query.price,
+            productImage: data.Location
+        });
+      await  product.save()
+            .then(result => {
+                res.status(200).send({
+                    _id: result._id,
+                    name: result.name,
+                    price: result.price,
+                    productImage: data.Location,
+                })
+            })
+            .catch(err => {
+                res.send({ message: err })
+          })
+    })
+})
 
-    res.status(200).json({ success: true, message: "Form submitted successfully" });
-  } catch (error) {
-    console.error("Error submitting Contact Us form:", error);
-    res.status(500).json({ success: false, message: "Internal server error" });
+// Get all the product data from db 
+
+   Aws.config.update({region: 'ap-south-1'})
+
+   const credentials = new Aws.Credentials({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET
+   })
+
+   Aws.config.credentials = credentials
+   const recognition = new Aws.Rekognition()
+ 
+
+  function compareFaces() {
+    const params = {
+        SourceImage: {
+          S3Object: {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Name: 'Aamir-Khan.png'
+          }
+        },
+        TargetImage: {
+          S3Object: {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Name: 'DepvgmaZ.jpg'
+          }
+        },
+        SimilarityThreshold: 97 // Adjust threshold as needed
+      };
+      
+    recognition.compareFaces(params, (err, data) => {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+      } else {
+        console.log(data); // successful response
+        if (data.FaceMatches.length > 0) {
+          const similarity = data.FaceMatches[0].Similarity;
+          console.log(`Faces are similar with similarity score: ${similarity}`);
+        } else {
+          console.log('Faces are not similar.');
+        }
+      }
+    });
   }
-});
+  
+  
+  compareFaces()
+
+  
+  
 
 
-// Endpoint for submitting the Contact Box form
-app.post("/submit-contactbox", async (req, res) => {
-  try {
-    const formData = req.body;
-    const referrerUrl = req.headers.referer || "Unknown"; // Get the referrer URL
-    const referrerDomain = url.parse(referrerUrl).hostname; // Extract the domain name from the URL
-    const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
 
-    // ... (Any additional validation or processing for Contact Box form data)
 
-    const mailOptions = {
-      from: "gautamsolar.vidoes01@gmail.com",  // sender email
-      to: "jokerkali99@gmail.com",  // another destination email
-      subject: "New Contact Box Form Submission",
-      html: `
-      <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
-      <h2 style="color: #a20000;">Enquiry Box Form Submission</h2>
-      <p style="margin-bottom: 10px;"><strong>Name:</strong> ${formData.name}</p>
-      <p style="margin-bottom: 10px;"><strong>Email:</strong> ${formData.email}</p>
-      <p style="margin-bottom: 10px;"><strong>Mobile No:</strong> ${formData.phone}</p>
-      <p style="margin-bottom: 10px;"><strong>State:</strong> ${formData.state}</p>
-      <p style="margin-bottom: 10px;"><strong>Willing to invest:</strong> ${formData.option}</p>
-      <p style="margin-bottom: 10px;"><strong>Remarks:</strong> ${formData.message}</p>
-      <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-    </div>
-      `,
-    };
 
-    await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ success: true, message: "Form submitted successfully" });
-  } catch (error) {
-    console.error("Error submitting Contact Box form:", error);
-    res.status(500).json({ success: false, message: "Internal server error" });
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, async()=>{
+  try{
+     await connect
+     console.log('Everything is running perfect')
+  }catch(err){
+console.log(err)
   }
-});
-// end point for solar plant form
-app.post("/submit-solarplant", async (req, res) => {
-  try {
-    const formData = req.body;
-    const referrerUrl = req.headers.referer || "Unknown"; // Get the referrer URL
-    const referrerDomain = url.parse(referrerUrl).hostname; // Extract the domain name from the URL
-    const referrerWebsite = extractWebsiteName(referrerDomain); // Extract the website name from the domain name
-
-    // ... (Any additional validation or processing for Contact Box form data)
-
-    const mailOptions = {
-      from: "gautamsolar.vidoes01@gmail.com",  // sender email
-      to: "jokerkali99@gmail.com",  // another destination email
-      subject: "solar plant Box Form Submission",
-      html: `
-      <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
-      <h2 style="color: #a20000;">solar plant Box Form Submission</h2>
-      <p style="margin-bottom: 10px;"><strong>Name:</strong> ${formData.name}</p>
-      <p style="margin-bottom: 10px;"><strong>Email:</strong> ${formData.email}</p>
-      <p style="margin-bottom: 10px;"><strong>Mobile No:</strong> ${formData.phone}</p>
-      <p style="margin-bottom: 10px;"><strong>Company Name:</strong> ${formData.company}</p>
-      <p style="margin-bottom: 10px;"><strong>Wants to put up:</strong> ${formData.option}</p>
-      <p style="margin-bottom: 10px;"><strong>Project Size:</strong> ${formData.sizeoption}</p>
-      <p style="margin-bottom: 10px;"><strong>Source:</strong> ${referrerWebsite}</p>
-    </div>
-      `,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    res.status(200).json({ success: true, message: "Form submitted successfully" });
-  } catch (error) {
-    console.error("Error submitting Contact Box form:", error);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+})
